c9c038146ca51376987bbb4948e0361e2ca4bf39
mateuszbroja <mat.broja@gmail.com>
2023-01-19 18:11:55 +0100

    added bigquery

--- README.md
+++ README.md
@@ -1,23 +1,20 @@
 # Poland IT Jobs Trends
 ## Architecture
 
-```bazaar
+### Ingestion
+
 Data sources:
 - justjoin.it
-- linkedin.com
 - nofluffjobs.pl
 
-Technologies:
+Front-end in JS
+
+All technologies used:
+- Python (library: pandas)
+- Google Cloud Platform:
+    - Cloud Run
+    - BigQuery
+    - Looker
+    - Scheduler
 - Docker
 - Terraform
-
-Google BigQuery
-Google Cloud Run
-
-Front-end in JS
-```
-
-```mermaid
-graph LR;
-A[Data Sources]--Cloud Run-->C[Google BigQuery]-->D[Website]
-```
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import time
-from helpers import read_yaml, scrape_data, flat_and_save_csv, json_to_string, upload_to_bucket
-
-# from tests import count_csv_records_and_columns
+from helpers import read_yaml, scrape_data, flat_json, upload_to_bucket,\
+                    upload_to_bigquery, execute_query
 
 config = read_yaml('docs/config.yaml')
 timestamp = time.strftime("%y%m%d_%H%M%S")
@@ -10,29 +9,23 @@ project_id = config['google_cloud']['project_id']
 credentials_path = config['google_cloud']['credentials_path']
 bucket_name = config['google_cloud']['bucket_name']
 
+
 for website_name, website in config['api_websites'].items():
-    # json_path = f"{website['output_path']}{timestamp}.json"
-    # csv_path = f"{website['output_path']}{timestamp}.csv"
-
-    # Request data from API
-    jobs_json = scrape_data(website['url'])
-    jobs_string = json_to_string(jobs_json)
-
-    # Load to Google Cloud Storage
-    upload_to_bucket(credentials_path, bucket_name, f'{timestamp}.json', jobs_string)
-
-    # Load to Google BigQuery
-    bronze_table_id = f'{project_id}.Landing.bronze_{website_name}'
-    uri = f'gs://landing_jobs/{timestamp}.json'
-    upload_to_bigquery(credentials_path, bronze_table_id, bucket_uri)
-
+    # Ingest data from APIs
     if website_name == 'nofluffjobs':
-        flat_and_save_csv(jobs_json['postings'], csv_path)
-    elif website_name == 'justjoinit':
-        flat_and_save_csv(jobs_json, csv_path)
+        jobs_json = scrape_data(website['url'])['postings']
+    else:
+        jobs_json = scrape_data(website['url'])
 
-    upload_to_bucket(credentials_path, bucket_name, f'{timestamp}.json', jobs_string)
-    # do some tests on the data
-    # create some metadata to all this
+    # Upload to the Google Cloud Storage
+    df_jobs = flat_json(jobs_json)
+    upload_to_bucket(credentials_path, bucket_name, f'{timestamp}.csv', df_jobs)
 
-upload_to_bigquery(credentials_path, bronze_table_id, uri)
+    # Load to the Google BigQuery - bronze
+    bronze_table_id = f'{project_id}.Landing.bronze_{website_name}'
+    query = f"TRUNCATE TABLE `{bronze_table_id}`"
+    execute_query(credentials_path, query)
+
+    uri = f'gs://landing_jobs/{timestamp}.csv'
+    upload_to_bigquery(credentials_path, bronze_table_id, uri)
+
